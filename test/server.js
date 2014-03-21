aa353a8ae1c6325b59749b7f62e0dffcc0402f93
Baptiste Fontaine <batifon@yahoo.fr>
2014-03-21 11:21:51 +0100

    more tests. Async tests don't run with Grunt.

--- test/server.js
+++ test/server.js
@@ -1,10 +1,29 @@
-// from https://github.com/carhartl/jquery-cookie/blob/master/test/server.js
+// based on https://github.com/carhartl/jquery-cookie/blob/master/test/server.js
 var http = require('http');
 var url  = require('url');
 var path = require('path');
 var fs   = require('fs');
 
+function setServerFlag( content, name ) {
+	if (/\.html$/.test(name)) {
+		return content.replace('/*SERVER_FLAG*/', 'window._in_browser = true;');
+	}
+	return content;
+}
+
 http.createServer(function(request, response) {
+
+	if (request.url == '/csrf') {
+		// AJAX endpoint. Returns the X-CSRFToken header
+		response.writeHead(200, { 'Content-Type': 'application/json' });
+		response.write(JSON.stringify({
+			present: request.headers.hasOwnProperty('x-csrftoken'),
+			value: request.headers['x-csrftoken']
+		}));
+		response.end();
+		return;
+	}
+
 	var uri = url.parse(request.url).pathname;
 	var filename = path.join(process.cwd(), uri);
 
@@ -16,10 +35,11 @@ http.createServer(function(request, response) {
 			return;
 		}
 
-		response.writeHead(200, filename.match(/\.js$/) ? { 'Content-Type': 'text/javascript' } : {});
-		response.write(file, 'utf-8');
+		response.writeHead(200, /\.js$/.test(filename) ? { 'Content-Type': 'text/javascript' } : {});
+		response.write(setServerFlag(file, filename), 'utf-8');
 		response.end();
 	});
+
 }).listen(8124, '0.0.0.0');
 
 console.log('Test suite at http://0.0.0.0:8124/test/index.html');
--- test/tests.js
+++ test/tests.js
@@ -1,6 +1,91 @@
-module('get value');
+if (typeof window !== 'object' || !window._in_browser) {
+	// asynchronous tests won't run with Grunt
+	asyncTest = function() {};
+}
 
-test('not enabled by default', function() {
-    expect(1);
-    strictEqual($.djangocsrf(), false);
-})
+module('enable', {
+	setup: function() {
+		$.djangocsrf( false );
+	}
+});
+
+test('with boolean', function() {
+	expect(1);
+	$.djangocsrf( true );
+	strictEqual($.djangocsrf(), true);
+});
+
+test('with "enable"', function() {
+	expect(1);
+	$.djangocsrf( 'enable' );
+	strictEqual($.djangocsrf(), true);
+});
+
+test('with "enabled"', function() {
+	expect(1);
+	$.djangocsrf( 'enabled' );
+	strictEqual($.djangocsrf(), true);
+});
+
+test('with trailing spaces', function() {
+	expect(1);
+	$.djangocsrf( ' enable   ' );
+	strictEqual($.djangocsrf(), true);
+});
+
+test('with uppercased letters', function() {
+	expect(1);
+	$.djangocsrf( 'EnabLe' );
+	strictEqual($.djangocsrf(), true);
+});
+
+asyncTest('should extract token from cookie', function() {
+	var tk = 'xea9$%' + (Math.random()*100|0);
+	expect(2);
+	$.djangocsrf( true );
+	$.cookie('csrftoken', tk);
+	$.ajax({
+		url: '/csrf',
+		success: function( data ) {
+			strictEqual(data.present, true);
+			strictEqual(data.value, tk);
+			start();
+		}
+	});
+});
+
+module('disable', {
+	setup: function() {
+		$.djangocsrf( true );
+	}
+});
+
+test('with boolean', function() {
+	expect(1);
+	$.djangocsrf( false );
+	strictEqual($.djangocsrf(), false);
+});
+
+test('with "disable"', function() {
+	expect(1);
+	$.djangocsrf( 'disable' );
+	strictEqual($.djangocsrf(), false);
+});
+
+test('with "disabled"', function() {
+	expect(1);
+	$.djangocsrf( 'disable' );
+	strictEqual($.djangocsrf(), false);
+});
+
+test('with trailing spaces', function() {
+	expect(1);
+	$.djangocsrf( ' disable   ' );
+	strictEqual($.djangocsrf(), false);
+});
+
+test('with uppercased letters', function() {
+	expect(1);
+	$.djangocsrf( 'DIsabLe' );
+	strictEqual($.djangocsrf(), false);
+});
